13ec4e6af92e95f19fa078c8f346effbca67190f
tvkain <tvkain.it@gmail.com>
2024-08-25 20:46:36 +0700

    update: max length to 2000

--- backend/src/documents/models.py
+++ backend/src/documents/models.py
@@ -19,12 +19,12 @@ class DocumentModel(SQLModel, table=True):
     id: Optional[str] = Field(
         default_factory=lambda: f"{uuid.uuid4()}", primary_key=True
     )
-    title: str
+    title: str | None = Field(default=None, max_length=2000)
     path: str | None = None
     thumbnail: str | None = None
     review_date: datetime | None = None
     editable: bool | None = None
-    summary: str | None = Field(default=None, max_length=16000)
+    summary: str | None = Field(default=None, max_length=2000)
     file_type: str | None = None
 
     user_id: str | None = Field(default=None, foreign_key="users.id")
