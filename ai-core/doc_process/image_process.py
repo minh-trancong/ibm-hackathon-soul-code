d1c9c677bd87751c6baa6127689c2c0ef4b3db89
Nguyễn Dũng <nqdhocai@gmail.com>
2024-08-25 20:00:50 +0700

    Add files via upload

--- ai-core/doc_process/image_process.py
+++ ai-core/doc_process/image_process.py
@@ -9,9 +9,9 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 async def get_img_detail(file_path):
     img = PIL.Image.open(file_path)
     prompt_title = "You are an expert in visual interpretation and title creation. Your task is to examine the provided image and generate a concise, descriptive title that captures the essence, theme, or most significant aspect of the image."
-    prompt_summary = "You are an expert in visual analysis and descriptive writing. Your task is to examine the provided image and compose a detailed description that captures its essential elements, mood, and context. The description should be approximately 200 words long and include information about the setting, subjects, colors, lighting, and any notable features or details that contribute to the overall impression of the image. Give me the short summary, less than 200 words"
+    prompt_summary = "You are an expert in visual analysis and descriptive writing. Your task is to examine the provided image and compose a detailed description that captures its essential elements, mood, and context. The description should be approximately 100 words long and include information about the setting, subjects, colors, lighting, and any notable features or details that contribute to the overall impression of the image. Give me the short summary, less than 100 words"
     title = model.generate_content([prompt_title, img]).text
     summary = model.generate_content([prompt_summary, img]).text
-    while len(summary) >= 4000:
-        summary = model.generate_content(["summary this sentence, it should be less than 300 words"]).text
+    if len(summary) >= 1000:
+        summary = summary[0:1000]
     return title, summary
